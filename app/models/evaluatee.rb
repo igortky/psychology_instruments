d0ee553a92b1807d34f374abc154331f14da5f16
Igor Labriola <igortky@gmail.com>
2024-04-13 15:36:51 -0300

    feat: validates to evaluatee model

--- app/models/evaluatee.rb
+++ app/models/evaluatee.rb
@@ -1,2 +1,6 @@
 class Evaluatee < ApplicationRecord
+  belongs_to :psychologist
+
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: true
+  validates :cpf, presence: true, uniqueness: true
 end
