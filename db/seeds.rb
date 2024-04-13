b892db706542460e5ee658fafb465e2f4bdfba27
Igor Labriola <igortky@gmail.com>
2024-04-13 16:53:47 -0300

    feat: add specs anda create factory

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,8 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Psychologist.create!(
+  first_name: 'Soren', last_name: 'Kierkegaard', email: 'admin@email.com',
+  password: 'password', password_confirmation: 'password'
+)
