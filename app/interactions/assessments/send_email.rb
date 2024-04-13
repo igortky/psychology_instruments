d90f8c163430425e9d99c85c77c75ec4e32011a5
Igor Labriola <igortky@gmail.com>
2024-04-13 19:12:48 -0300

    feat: add intrstument_id

--- app/interactions/assessments/send_email.rb
+++ app/interactions/assessments/send_email.rb
@@ -1,9 +1,12 @@
 module Assessments
   class SendEmail < ActiveInteraction::Base
+    include Rails.application.routes.url_helpers
+
     object :evaluatee, class: ::Evaluatee
+    integer :instrument_id
 
     def execute
-      AssessmentMailer.with(evaluatee:)
+      AssessmentMailer.with(evaluatee:, instrument_id:)
                       .send_instrument_to_evaluatee
                       .deliver_later
     end
