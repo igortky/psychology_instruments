e3937ec9596d02dbdced3a4f93c50528f42b1e3c
Igor Labriola <igortky@gmail.com>
2024-04-13 15:46:36 -0300

    feat: add validate for CPF

--- app/models/evaluatee.rb
+++ app/models/evaluatee.rb
@@ -2,5 +2,5 @@ class Evaluatee < ApplicationRecord
   belongs_to :psychologist
 
   validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: true
-  validates :cpf, presence: true, uniqueness: true
+  validates :cpf, presence: true, uniqueness: true, length: { is: 11 }, format: { with: /\A[0-9]+\z/ }
 end
