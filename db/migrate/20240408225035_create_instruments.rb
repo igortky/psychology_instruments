87f545b5909e32d42aae416d3a6f1d2faeacc055
Igor Labriola <igortky@gmail.com>
2024-04-14 11:24:56 -0300

    feat: update instrument model

--- db/migrate/20240408225035_create_instruments.rb
+++ db/migrate/20240408225035_create_instruments.rb
@@ -2,6 +2,8 @@ class CreateInstruments < ActiveRecord::Migration[7.1]
   def change
     create_table :instruments do |t|
       t.string :name
+      t.jsonb :questions, default: [], null: false
+      t.string :status, default: 'open'
 
       t.timestamps
     end
