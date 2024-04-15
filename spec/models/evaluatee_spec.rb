36e9c28df7dd10a0faa807309c93a81b8670a119
Igor Labriola <igortky@gmail.com>
2024-04-15 06:40:50 -0300

    feat: evaluatee model spec

--- spec/models/evaluatee_spec.rb
+++ spec/models/evaluatee_spec.rb
@@ -1,5 +1,23 @@
+# spec/models/evaluatee_spec.rb
 require 'rails_helper'
 
 RSpec.describe Evaluatee, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to(:psychologist) }
+  it { should have_many(:assessments) }
+  it { should have_many(:instruments).through(:assessments) }
+
+  describe 'email validations' do
+    subject { Evaluatee.new(cpf: '12345678902', email: 'test1@example.com') }
+    it { should validate_presence_of(:email) }
+    it { should allow_value('test@example.com').for(:email) }
+    it { should_not allow_value('invalid_email').for(:email) }
+  end
+
+  describe 'cpf validations' do
+    subject { Evaluatee.new(cpf: '12345678903', email: 'test2@example.com') }
+    it { should validate_presence_of(:cpf) }
+    it { should validate_length_of(:cpf).is_equal_to(11) }
+    it { should allow_value('12345678901').for(:cpf) }
+    it { should_not allow_value('1234567890a').for(:cpf) }
+  end
 end
